83bc202de3abe24ec01d44df70d809b2c0e8e264
DajeRiman <121157487+Veeradel@users.noreply.github.com>
2024-03-03 20:44:03 +0100

    prev next days

--- src/main/java/com/example/registro/HomeStudenti.java
+++ src/main/java/com/example/registro/HomeStudenti.java
@@ -5,24 +5,51 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HomeStudenti {
 
     @FXML
-    private Label data;
+    private Label numeroGiorno;
+    @FXML
+    private Label giorno;
+    @FXML
+    private Label meseAnno;
+
+    private LocalDate currentDate;
 
     @FXML
     public void initialize() {
-        LocalDate oggi = LocalDate.now();
-        int giorno = oggi.getDayOfMonth();
-        String giornoSettimana = oggi.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
-        int mese = oggi.getMonthValue();
-        String nomeMese = oggi.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
-        int anno = oggi.getYear();
+        currentDate = LocalDate.now();
+        updateLabels();
+    }
 
-        String formattedDate = String.format("%d %s %s %d", giorno, giornoSettimana, nomeMese, anno);
-        data.setText("Data: " + formattedDate);
+    private void updateLabels() {
+        int giornoNumero = currentDate.getDayOfMonth();
+        String giornoSettimana = currentDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        int mese = currentDate.getMonthValue();
+        String nomeMese = currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        int anno = currentDate.getYear();
+
+        String numeroGiornostr = String.format("%d ", giornoNumero);
+        numeroGiorno.setText(numeroGiornostr);
+        String giornostr = String.format("%s ", giornoSettimana);
+        giorno.setText(giornostr);
+        String meseAnnostr = String.format("%s %d ", nomeMese, anno);
+        meseAnno.setText(meseAnnostr);
+    }
+
+    @FXML
+    void nextDay(ActionEvent event) {
+        currentDate = currentDate.plusDays(1);
+        updateLabels();
+    }
+
+    @FXML
+    void prevDay(ActionEvent event) {
+        currentDate = currentDate.minusDays(1);
+        updateLabels();
     }
 }
